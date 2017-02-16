b52e5f1f044ad9815a059986e86f42686e57d3b2
Johanna <Johanna@JASPER-PC>
2017-02-16 17:59:52 +0200

    Add project files.

---
+++ PointCustomSystemDataMVC/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace PointCustomSystemDataMVC
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ PointCustomSystemDataMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PointCustomSystemDataMVC.Startup))]
+namespace PointCustomSystemDataMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
